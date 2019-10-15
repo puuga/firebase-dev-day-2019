e96881c20f4e120b9ed9a53e595883f1ab3b9e0d
Siwawes Wongcharoen <siwaoh@gmail.com>
2019-10-15 21:24:57 +0700

    Update data

--- data.php
+++ data.php
@@ -11,19 +11,34 @@ $data = [
         'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F53678154_2220403671544055_295646036620214272_o.jpg?alt=media&token=343afc80-86a6-4c01-b97f-cccdab4bc1f4'
     ],
     [
-        'name' => 'การ์ตูน บั่นทอน 2',
-        'ref' => 'https://www.facebook.com/cartoonbuntorn',
-        'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F53678154_2220403671544055_295646036620214272_o.jpg?alt=media&token=343afc80-86a6-4c01-b97f-cccdab4bc1f4'
+        'name' => 'เรื้อน',
+        'ref' => 'https://www.facebook.com/i.ruean/',
+        'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F71954537_1236662539875772_4724486922070654976_o.jpg?alt=media&token=da78a61c-233b-42fb-9e33-abc79adcd708'
     ],
     [
-        'name' => 'การ์ตูน บั่นทอน 3',
-        'ref' => 'https://www.facebook.com/cartoonbuntorn',
-        'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F53678154_2220403671544055_295646036620214272_o.jpg?alt=media&token=343afc80-86a6-4c01-b97f-cccdab4bc1f4'
+        'name' => 'สมปอนด์',
+        'ref' => 'https://www.facebook.com/sompondofficial/',
+        'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F48365228_1948352115251191_3615049014988242944_o.png?alt=media&token=09b92a44-2d8d-41b3-a757-9bd641e3b3df'
     ],
     [
-        'name' => 'การ์ตูน บั่นทอน 4',
-        'ref' => 'https://www.facebook.com/cartoonbuntorn',
-        'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F53678154_2220403671544055_295646036620214272_o.jpg?alt=media&token=343afc80-86a6-4c01-b97f-cccdab4bc1f4'
+        'name' => 'จ๊ากกก',
+        'ref' => 'https://www.facebook.com/%E0%B8%88%E0%B9%8A%E0%B8%B2%E0%B8%81%E0%B8%81%E0%B8%81-1106806269514825/',
+        'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F62116107_1109610289234423_7539993750177579008_n.jpg?alt=media&token=1a25f750-ccad-4b74-b8de-f88bbe0e5750'
+    ],
+    [
+        'name' => 'พ่อบ้านใจกล้า',
+        'ref' => 'https://www.facebook.com/powerofhusbands/',
+        'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F19800899_765638090307535_382280591469029239_o.jpg?alt=media&token=e2464529-003e-4e99-a158-8c50febd5ca6'
+    ],
+    [
+        'name' => 'Movie Gag',
+        'ref' => 'https://www.facebook.com/movieg4g/',
+        'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F72690857_2375794826070958_7720727790764425216_n.jpg?alt=media&token=d23d8366-9018-4495-a45d-454dc5185b4f'
+    ],
+    [
+        'name' => 'กาตูนร์ ระทม',
+        'ref' => 'https://www.facebook.com/cartoonratom/',
+        'image_url' => 'https://firebasestorage.googleapis.com/v0/b/gear2019v2.appspot.com/o/firebase-dev-day-2019%2F56915089_2083586095072406_4110449513779429376_n.jpg?alt=media&token=68579370-4754-48cf-a4ac-1a9c6dabbf1d'
     ],
 ];
 ?>
